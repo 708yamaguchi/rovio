694ae9e5be419004c5c38c92c288043a57f12213
Naoya Yamaguchi <708yamaguchi@gmail.com>
2024-03-24 15:02:13 +0900

    Fix gyro unit: deg/s -> rad/s

--- node_scripts/MPU6886.py
+++ node_scripts/MPU6886.py
@@ -7,6 +7,7 @@ import board
 import busio
 import time
 
+import numpy as np
 import rospy
 from sensor_msgs.msg import Imu
 
@@ -138,9 +139,9 @@ class PublishMPU6886(object):
         imu_msg.header.frame_id = 'imu_frame'
         gx, gy, gz = self.m.gyro()
         ax, ay, az = self.m.acceleration()
-        imu_msg.angular_velocity.x = gx
-        imu_msg.angular_velocity.y = gy
-        imu_msg.angular_velocity.z = gz
+        imu_msg.angular_velocity.x = np.deg2rad(gx)
+        imu_msg.angular_velocity.y = np.deg2rad(gy)
+        imu_msg.angular_velocity.z = np.deg2rad(gz)
         imu_msg.linear_acceleration.x = ax
         imu_msg.linear_acceleration.y = ay
         imu_msg.linear_acceleration.z = az
--- node_scripts/imu_convert.py
+++ node_scripts/imu_convert.py
@@ -2,16 +2,18 @@
 
 import rospy
 from sensor_msgs.msg import Imu
+import numpy as np
 
 
 def cb(msg):
     imu_converted = msg
-    imu_converted.angular_velocity.x = -1 * imu_converted.angular_velocity.x
-    imu_converted.angular_velocity.y = -1 * imu_converted.angular_velocity.y
-    imu_converted.angular_velocity.z = -1 * imu_converted.angular_velocity.z
-    imu_converted.linear_acceleration.x = -1 * imu_converted.linear_acceleration.x
-    imu_converted.linear_acceleration.y = -1 * imu_converted.linear_acceleration.y
-    imu_converted.linear_acceleration.z = -1 * imu_converted.linear_acceleration.z
+
+    imu_converted.angular_velocity.x = np.deg2rad(imu_converted.angular_velocity.x)
+    imu_converted.angular_velocity.y = np.deg2rad(imu_converted.angular_velocity.y)
+    imu_converted.angular_velocity.z = np.deg2rad(imu_converted.angular_velocity.z)
+    # imu_converted.linear_acceleration.x = -1 * imu_converted.linear_acceleration.x
+    # imu_converted.linear_acceleration.y = -1 * imu_converted.linear_acceleration.y
+    # imu_converted.linear_acceleration.z = -1 * imu_converted.linear_acceleration.z
     pub.publish(imu_converted)
 
 
